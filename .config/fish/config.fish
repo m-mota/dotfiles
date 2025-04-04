10374841208b259c7f9b7b36c5e9ce58bfbee5a4
marco <emicepeemi@gmail.com>
2025-04-04 13:57:36 -0300

    Minimalized fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,36 +2,31 @@ set fish_greeting
 
 
 function fish_prompt
-    # Max directory name length
-    set max 255
+    set max_len 255
 
     # From Catppuccin Mocha
     set home cba6f7 # Mauve
     set root f9e2af # Yellow
 
-    set pwd (prompt_pwd -d $max)
-    set env (string sub -s 1 -l 1 $pwd)
+    set dir (prompt_pwd -d $max_len)
+
+    set env (string sub -s 1 -l 1 $dir)
     if test $env = "~"
-        set color $home
+	set color $home
     else
 	set color $root
     end
 
-    set dir (string split "/" (string sub -s 2 $pwd))
-    set child $dir[-1]
-    set parent $dir[-2]
+    # Split away anchor
+    set folder (string split "/" (string sub -s 2 $dir))[-1]
 
-    if test -z $parent
-	set parent $env
-    end
-    if test -z $child
-	set prompt $parent
+    if test -z $folder
+	set prompt $env
     else
-	set prompt "$parent/$child"
+	set prompt $folder
     end
-    set prompt (string replace -r "//" "/" $prompt)
 
-    echo \n (set_color -o $color) $prompt "" (set_color normal)
+    echo \n (set_color -o $color) $prompt (set_color normal) ""
 end
 
 

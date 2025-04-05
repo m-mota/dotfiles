fa9adb0cad43e472a38b995323d2bc88b085ca77
marco <emicepeemi@gmail.com>
2025-04-05 13:15:49 -0300

    Refactored Fish config... again...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,30 +2,10 @@ set fish_greeting
 
 
 function fish_prompt
-    set max_len 255
+    set hex cba6f7 # Catppuccin Mocha Mauve
+    set dir (basename (prompt_pwd))
 
-    # From Catppuccin Mocha
-    set home cba6f7 # Mauve
-    set root f9e2af # Yellow
-
-    set dir (prompt_pwd -d $max_len)
-
-    set env (string sub -s 1 -l 1 $dir)
-    if test $env = "~"
-	set color $home
-    else
-	set color $root
-    end
-
-    set folder (string split "/" (string sub -s 2 $dir))[-1]
-
-    if test -z $folder
-	set prompt $env
-    else
-	set prompt $folder
-    end
-
-    echo \n (set_color -o $color) $prompt (set_color normal) ""
+    echo \n (set_color -o $hex) $dir (set_color normal) ""
 end
 
 

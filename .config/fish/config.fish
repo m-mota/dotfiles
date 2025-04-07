baad851df95eb3121588a7a2a3bf3822495bd9d9
marco <emicepeemi@gmail.com>
2025-04-07 15:51:37 -0300

    Tweaked Kitty font and Fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,9 @@ set fish_greeting
 
 
 function fish_prompt
-    set hex cba6f7 # Catppuccin Mocha Mauve
-    set dir (basename (prompt_pwd))
-
-    echo \n (set_color -o $hex) $dir (set_color normal) ""
+    # set prompt "$USER@$(prompt_hostname) $(basename (prompt_pwd))"
+    set prompt (basename (prompt_pwd))
+    echo (set_color -r yellow) $prompt (set_color normal) ""
 end
 
 
